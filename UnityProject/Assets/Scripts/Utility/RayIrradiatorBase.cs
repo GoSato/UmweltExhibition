f6b43b0c6c1e1889d54b7147c286f3e423c933e5
GO <go05sato@gmail.com>
2017-06-28 04:23:17 +0900

    add RayIrradiatorBase base class

---
+++ UnityProject/Assets/Scripts/Utility/RayIrradiatorBase.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RayIrradiatorBase : MonoBehaviour
+{
+    protected GameObject _hitObj;
+    protected Vector3 _hitPoint;
+   
+    private void FixedUpdate()
+    {
+        var fwd = transform.TransformDirection(Vector3.forward);
+        RaycastHit hit;
+        int layerMask = LayerMask.GetMask(new string[] { "RayHittable" });
+
+        Debug.DrawLine(transform.position, transform.position + fwd * 100, Color.red);
+        
+        if(Physics.Raycast(transform.position, fwd, out hit, Mathf.Infinity, layerMask))
+        {
+            _hitObj = hit.collider.gameObject;
+            _hitPoint = hit.point;
+        }
+        else
+        {
+            _hitObj = null;
+            _hitPoint = Vector3.zero;
+        }
+    }
+}
