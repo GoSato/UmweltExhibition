c09cace6c40ce9f455a42ae7a35e40ceb2954d18
GO <go05sato@gmail.com>
2017-06-28 04:22:50 +0900

    add SingletonMonoBehaviour base class

---
+++ UnityProject/Assets/Scripts/Utility/SingletonMonoBehavior.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using UnityEngine;
+
+public class SingletonMonoBehavior<T> : MonoBehaviour where T : MonoBehaviour {
+
+    private static T instance;
+
+	public static T Instance
+    {
+        get
+        {
+            if(instance == null)
+            {
+                Type t = typeof(T);
+
+                instance = (T)FindObjectOfType(t);
+
+                if (instance == null)
+                {
+                    Debug.LogError("No object has " + t);
+                }
+            }
+
+            return instance;
+        }
+    }
+
+    virtual protected void Awake()
+    {
+        if(this != Instance)
+        {
+            Destroy(this);
+            return;
+        }
+    }
+}
