9c490de6d117c0db9da8e9f2667ee9fe66944fe4
GO <go05sato@gmail.com>
2017-06-28 04:23:54 +0900

    Camera Position Change Class

---
+++ UnityProject/Assets/Scripts/Teleporter.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Teleporter : RayIrradiatorBase
+{
+    void Update()
+    {
+        if(_hitObj != null)
+        {
+            // TODO : This is test input function.
+            if(Input.GetKeyDown(KeyCode.Space))
+            {
+                transform.position = _hitObj.transform.position;
+            }
+        }
+    }
+}
