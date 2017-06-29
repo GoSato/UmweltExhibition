b2434c9873696d701af0811d6bb38551e3997641
GO <go05sato@gmail.com>
2017-06-30 03:24:27 +0900

    add Tester

---
+++ UnityProject/Assets/Scripts/Tester/VideoTester.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VideoTester : MonoBehaviour
+{
+    private VideoPlayerBehaviour _videoPlayer;
+
+    void Start()
+    {
+        _videoPlayer = FindObjectOfType<VideoPlayerBehaviour>();
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            _videoPlayer.PlayMove();
+        }
+    }
+}
