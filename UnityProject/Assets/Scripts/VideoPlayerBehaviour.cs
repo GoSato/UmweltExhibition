e02f88c46095c06aa12509aecf32f5fe453016e7
GO <go05sato@gmail.com>
2017-06-30 03:24:17 +0900

    create Video Player Script

---
+++ UnityProject/Assets/Scripts/VideoPlayerBehaviour.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+
+[RequireComponent(typeof(VideoPlayer))]
+public class VideoPlayerBehaviour : MonoBehaviour
+{
+    private VideoPlayer _video;
+
+    void Start()
+    {
+        _video = GetComponent<VideoPlayer>();
+    }
+
+    public void PlayMove()
+    {
+        _video.Play();
+    }
+}
