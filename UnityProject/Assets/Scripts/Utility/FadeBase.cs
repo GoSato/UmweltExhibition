56a950eb701c80e3234d5c87ae075593b2700250
GO <go05sato@gmail.com>
2017-07-01 06:37:08 +0900

    create FadeBase class

---
+++ UnityProject/Assets/Scripts/Utility/FadeBase.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FadeBase : MonoBehaviour
+{
+    [SerializeField]
+    private float _fadeOutTime;
+    [SerializeField]
+    private float _fadeInTime;
+
+    protected Timer _timer = new Timer();
+    private Delegate.VoidDelegate FadeImpl;
+
+    private bool _isEnableFade = false;
+    public bool IsEnableFade { get { return _isEnableFade; } }
+
+    public virtual void Update()
+    {
+        if (_isEnableFade)
+        {
+            if (_timer.Update())
+            {
+                _isEnableFade = false;
+            }
+            else
+            {
+                FadeImpl();
+            }
+        }
+    }
+
+    /// <summary>
+    /// フェード処理開始
+    /// </summary>
+    public void FadeStart(FadeType type)
+    {
+        switch(type)
+        {
+            case FadeType.FadeOut:
+                _timer.LimitTime = _fadeOutTime;
+                FadeImpl = FadeOut;
+                break;
+            case FadeType.FadeIn:
+                _timer.LimitTime = _fadeInTime;
+                FadeImpl = FadeIn;
+                break;
+            default:
+                break;
+        }
+           
+        _isEnableFade = true;
+    }
+
+    /// <summary>
+    /// フェードアウト
+    /// </summary>
+    public virtual void FadeOut()
+    {
+    }
+
+    /// <summary>
+    /// フェードイン
+    /// </summary>
+    public virtual void FadeIn()
+    {
+    }
+}
+
+public enum FadeType
+{
+    FadeOut, FadeIn
+}
