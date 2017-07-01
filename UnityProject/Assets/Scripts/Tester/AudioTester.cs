85f9518098869b625215af3ca6ea0b6bf8d1f980
GO <go05sato@gmail.com>
2017-07-01 14:23:22 +0900

    add testcode

---
+++ UnityProject/Assets/Scripts/Tester/AudioTester.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioTester : MonoBehaviour
+{
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            AudioController.Instance.PlayAll();
+        }
+    }
+}
