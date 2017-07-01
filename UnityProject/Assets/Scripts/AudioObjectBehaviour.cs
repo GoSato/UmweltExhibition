1bfd77f5181c6c309fae6451ef4002551faf777b
GO <go05sato@gmail.com>
2017-07-01 17:17:53 +0900

    create AudioObjectBehaviour

---
+++ UnityProject/Assets/Scripts/AudioObjectBehaviour.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioObjectBehaviour : MonoBehaviour
+{
+    [SerializeField]
+    private FrequencyType _type;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (IsPlayer(other))
+        {
+            AudioController.Instance.ChangeSourceType(_type, SourceType.Mono);
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if(IsPlayer(other))
+        {
+            AudioController.Instance.ChangeSourceType(_type, SourceType.Stereo);
+        }
+    }
+
+    private bool IsPlayer(Collider other)
+    {
+        if(other.tag == "Player")
+        {
+            return true;
+        }
+        return false;
+    }
+}
