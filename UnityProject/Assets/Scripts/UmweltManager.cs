666d54e33d9d9212e24c3987423f2ffd3d405333
GO <go05sato@gmail.com>
2017-07-01 15:05:48 +0900

    create UmweltManager

---
+++ UnityProject/Assets/Scripts/UmweltManager.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UmweltManager : GameManagerBase
+{
+    private SkyboxFader _skyboxFader;
+
+    public override void Start()
+    {
+        base.Start();
+        _skyboxFader = FindObjectOfType<SkyboxFader>();
+    }
+
+    public override void DoStartAction()
+    {
+        base.DoStartAction();
+    }
+
+    public override void DoPrepareAction()
+    {
+        base.DoPrepareAction();
+    }
+
+    public override void DoPlayingAction()
+    {
+        base.DoPlayingAction();
+        AudioController.Instance.PlayAll();
+        _skyboxFader.FadeStart(FadeType.FadeIn);
+    }
+
+    public override void DoEndAction()
+    {
+        base.DoEndAction();
+        AudioController.Instance.StopAll();
+        _skyboxFader.FadeStart(FadeType.FadeOut);
+    }
+}
