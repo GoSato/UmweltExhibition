82eadf3178f33f3457f3e11162168f6751379cfe
GO <go05sato@gmail.com>
2017-07-06 01:50:05 +0900

    add interface

--- Umwelt_Edited/Assets/_GO/Scripts/AudioObjectBehaviour.cs
+++ Umwelt_Edited/Assets/_GO/Scripts/AudioObjectBehaviour.cs
@@ -1,31 +1,60 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class AudioObjectBehaviour : MonoBehaviour
+public class AudioObjectBehaviour : MonoBehaviour, IAudioObject
 {
     [SerializeField]
     private FrequencyType _type;
+
+    public FrequencyType Type
+    {
+        get
+        {
+            return _type;
+        }
+
+        set
+        {
+
+        }
+    }
+
+    private RawImage _image;
+    public RawImage Image
+    {
+        get
+        {
+            return _image;
+        }
+
+        set
+        {
+            _image = value;
+        }
+    }
 
     private void OnTriggerEnter(Collider other)
     {
         if (IsPlayer(other))
         {
-            AudioController.Instance.ChangeSourceType(_type, SourceType.Mono);
+            //AudioController.Instance.ChangeSourceType(_type, SourceType.Mono);
         }
     }
 
     private void OnTriggerExit(Collider other)
     {
-        if(IsPlayer(other))
+        if (IsPlayer(other))
         {
-            AudioController.Instance.ChangeSourceType(_type, SourceType.Stereo);
+            //AudioController.Instance.ChangeSourceType(_type, SourceType.Stereo);
         }
     }
 
     private bool IsPlayer(Collider other)
     {
-        if(other.tag == "Player")
+        if (other.tag == "Player")
         {
             return true;
         }
